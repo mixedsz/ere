b18b76bc803a619d5d80eda15903807833b865ff
mixedsz <treygoodlik@gmail.com>
2026-02-08 17:53:19 -0500

    Delete coinShop directory

--- coinShop/data/language.lua
+++
@@ -1,9 +0,0 @@
-
-
-Language = {
-    ['cooldown'] = 'Please wait before performing this action again!',
-    ['invalid_code'] = 'This code does not exist! Please contact the owners if you think this is a mistake!',
-    ['valid_code'] = 'WOW! You have just received %d coins ✨',
-    ['not_enough_coins'] = 'You do not have enough coins to purchase this item!',
-    ['no_role'] = 'You do not have the role required to purchase this item!'
- }
